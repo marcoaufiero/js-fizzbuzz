e4351e9b4c3ed53274e6432fed87425ab01e7180
marcoaufiero <marcoaufiero96@gmail.com>
2022-10-21 15:32:01 +0200

    js : lists added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,15 @@ ul.append(li);
 for( let i=1; i <= 100; i++){
     
     if( (i % 3) == 0 && (i % 5) == 0 ){
-        li.innerText += 'Fizzbuzz'
+        li.innerHTML += '<li>Fizzbuzz</li>'
     
     } else if( i % 3 == 0){
-        li.innerText += 'Fizz'
+        li.innerHTML += '<li>Fizz</li>'
     
     } else if( i % 5 == 0){
-        li.innerText += 'Buzz'
+        li.innerHTML += '<li>Buzz</li>'
     
     } else{
-        li.innerText += `${i}`
+        li.innerHTML += `<li>${i}</li>`
     }
 }
